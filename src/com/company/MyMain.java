0bae5419f84600edeaf4b374a5654779e92dd2e5
Mohammed Shinoy <Shinz4u@gmail.com>
2016-09-28 01:30:28 -0400

    Adding ArrayLeftRotation and Recursion Classes.

--- src/com/company/MyMain.java
+++ src/com/company/MyMain.java
@@ -4,8 +4,16 @@ import java.util.*;
 public class MyMain {
 
     public static void main(String[] args) {
-        PrimeNumbers prime = new PrimeNumbers();
-        prime.printSumofPrime(1000000);
+        //PrimeNumbers prime = new PrimeNumbers();
+        //prime.printSumofPrime(1000000);
+        Recursion fact = new Recursion();
+        //System.out.println("Factorial of number 4 is "+ fact.factorial(4));
+        int num = 10;
+        for (int i = 1; i <= 10; i++){
+            System.out.println(fact.fibonacci(10) + " ");
+        }
+
+
 
 
 
